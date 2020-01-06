efe7acb13f235d0c30ecb88562c5ac729f5968c7
sunecosuri <sunecosuri@gmail.com>
2020-01-06 16:17:38 +0900

    add Exercise1

--- react-suburi/src/App.tsx
+++ react-suburi/src/App.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type HelloProps = {
+  name: string
+  age: number
+}
+const Hello = (props: HelloProps) => {
+  return (
+    <div>
+      <p>Hello { props.name }</p>
+      <p>{ props.age } years old</p>
+    </div>
+  )
+}
 const App: React.FC = () => {
+  const now = new Date()
+  const age = 10
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {now.toDateString()}
+        </p>
+        <Hello name="test" age={3} />
+        <Hello name="sample" age={age} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -20,7 +40,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
